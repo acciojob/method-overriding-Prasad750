f4868b7a4564606959f007fdd1697674ef621768
Prasad750 <prasadchaudhari750@gmail.com>
2023-08-09 15:24:08 +0530

    Attempt2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,9 +2,18 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
-        B b=new B();
-        String str=b.meth();
+
+        B objb=new B();
+
+        //calling method of A class from object of B
+        String str=objb.meth();
         System.out.println(str);
+
+        // calling class A overridden method from B
+        String str1=objb.meth();
+        System.out.println(str1);
+
+
     }
   
 }
